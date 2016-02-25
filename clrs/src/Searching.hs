22994d8141ca9e3305a9507cf1c7b9f4bf59f412
Chris Coffey <cmcof273@gmail.com>
2016-02-25 08:49:21 -0500

    simple binary search

--- clrs/src/Searching.hs
+++ clrs/src/Searching.hs
@@ -15,3 +15,16 @@ linearSearchI pred as = f 0 as where
     f n (x:xs)
         | pred x    = Just n
         | otherwise = f (n + 1) xs
+
+binarySearch :: Ord a =>  a -> [a] -> Maybe a
+binarySearch seeking (a:[])
+    | a == seeking  = Just a
+    | otherwise     = Nothing
+binarySearch seeking ls = let 
+    mi = (length ls) `div` 2
+    mid = ls !! mi
+    in if mid == seeking
+       then Just mid
+       else if mid < seeking
+            then binarySearch seeking (drop mi ls)
+            else binarySearch seeking (take mi ls)
